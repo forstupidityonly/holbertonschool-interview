1550ad1685e694463a3b2b2af667c7df81934c89
Corbin <forstupidityonly@gmail.com>
2021-07-22 13:07:40 -0500

    not sure how but indentation changed, pepfix and push

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """lockboxes"""
 
+
 def canUnlockAll(boxes):
     """Open all boxes with keys from other boxes. Keys and boxes are represented
        as numbers: #1 key opens #1 box"""
-    key_chain = [0]    #Always have key to box-0
+    key_chain = [0]  # Always have key to box-0
     opened_boxes = []
-        if not boxes:
+    if not boxes:
         return False
 
-    while key_chain:    #While there are unused keys
-        key = key_chain.pop()    #Remove top key
-        if key not in opened_boxes:    #If not a duplicate, use key
-            opened_boxes.append(key)    #Track it's used
-            for i in boxes[key]:    #For key in opend box
-                if i < len(boxes):    #Check the range is valid
-                    key_chain.append(i)    #Add to keychain
+    while key_chain:  # While there are unused keys
+        key = key_chain.pop()  # Remove top key
+        if key not in opened_boxes:  # If not a duplicate, use key
+            opened_boxes.append(key)  # Track it's used
+            for i in boxes[key]:  # For key in opend box
+                if i < len(boxes):  # Check the range is valid
+                    key_chain.append(i)  # Add to keychain
     if len(opened_boxes) == len(boxes):
-        return True    #All boxes have been opened
+        return True  # All boxes have been opened
     else:
-        return False    #Boxes left without keys
+        return False  # Boxes left without keys
