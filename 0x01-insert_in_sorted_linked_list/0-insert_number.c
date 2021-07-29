bcbd34a134d357d92b3abfbe0c145ded49725798
Corbin <forstupidityonly@gmail.com>
2021-07-29 12:41:46 -0500

    one node ptr use ->next->next

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -7,37 +7,28 @@
   */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *node_ptr_1 = *head;
-    listint_t *node_ptr_2 = (*head)->next;
-    listint_t *new_node;
-    
-    if (!head || !number)
-        return (NULL);
-    new_node = malloc(sizeof(listint_t));
+    listint_t *node_ptr = *head;
+    listint_t *new_node = malloc(sizeof(listint_t));
+
     if (!new_node)
         return (NULL);
-    else
-        new_node->n = number;
-    if (node_ptr_1->n > number)
-    {
-        new_node->next = node_ptr_1;
-        return (new_node);
-    }
-    while (node_ptr_2)
-    {
-        if (node_ptr_1->n < number && node_ptr_2->n > number)
-        {
-            node_ptr_1->next = new_node;
-            new_node->next = node_ptr_2;
-            return (new_node);
-        }
-        else
-        {
-            node_ptr_1 = node_ptr_1->next;
-            node_ptr_2 = node_ptr_2->next;
-        }
-    }
-    node_ptr_1->next = new_node;
+    new_node->n = number;
     new_node->next = NULL;
-    return (new_node);
+    if (!node_ptr || new_node->n < node_ptr->n)
+    {
+        new_node->next = node_ptr;
+        *head = new_node;
+        return (*head);
+    }
+    while (node_ptr)
+    {
+        if (!node_ptr->next || new_node->n < node_ptr->next->n)
+        {
+            new_node->next = node_ptr->next;
+            node_ptr->next = new_node;
+            return (node_ptr);
+        }
+        node_ptr = node_ptr->next;
+    }
+    return (NULL);
 }
