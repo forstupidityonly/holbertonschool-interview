1ce3865ea86a8d647bacecd42e8a3430263c05e9
Corbin <forstupidityonly@gmail.com>
2021-10-07 11:14:40 -0500

    vscode and betty are best friends

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -2,17 +2,17 @@
 #include <math.h>
 #include "menger.h"
 /**
-  * charCalc - maths what char to print block or no block
-  * @i: height
-  * @j: width
-  * Return: ' ', '#'
-  */
+* charCalc - maths what char to print block or no block
+* @i: height
+* @j: width
+* Return: ' ', '#'
+*/
 char charCalc(int i, int j)
 {
-    for (; i && j; i /= 3, j /= 3)
-        if (i % 3 == 1 && j % 3 == 1)
-            return (' ');
-    return ('#');
+	for (; i && j; i /= 3, j /= 3)
+		if (i % 3 == 1 && j % 3 == 1)
+			return (' ');
+	return ('#');
 }
 /**
  * menger - draws a 2D Menger Sponge
@@ -20,15 +20,15 @@ char charCalc(int i, int j)
  */
 void menger(int level)
 {
-    int size, i, j;
+	int size, i, j;
 
-    if (level < 0)
-        return;
-    size = pow(3, level);
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-            printf("%c", charCalc(i, j));
-        printf("\n");
-    }
+	if (level < 0)
+		return;
+	size = pow(3, level);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			printf("%c", charCalc(i, j));
+		printf("\n");
+	}
 }
