f87ab5ba381daa945d8ac6476975844e32e341fd
Corbin <forstupidityonly@gmail.com>
2021-10-07 11:09:49 -0500

    i knew i needed to /3 and %3 once i guessed the right order to do those in it worked, i dont quite understand how but i dont care

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include "menger.h"
+/**
+  * charCalc - maths what char to print block or no block
+  * @i: height
+  * @j: width
+  * Return: ' ', '#'
+  */
+char charCalc(int i, int j)
+{
+    for (; i && j; i /= 3, j /= 3)
+        if (i % 3 == 1 && j % 3 == 1)
+            return (' ');
+    return ('#');
+}
+/**
+ * menger - draws a 2D Menger Sponge
+ * @level: level to draw
+ */
+void menger(int level)
+{
+    int size, i, j;
+
+    if (level < 0)
+        return;
+    size = pow(3, level);
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+            printf("%c", charCalc(i, j));
+        printf("\n");
+    }
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,6 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+void menger(int level);
+char charCalc(int i, int j);
+#endif
