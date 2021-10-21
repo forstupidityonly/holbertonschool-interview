46ec911409d69da2e5a98c50e257459afae1a0f4
Corbin <forstupidityonly@gmail.com>
2021-10-21 15:01:39 -0500

    what have i dont wrong lmao

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""N queens """
 import sys
 
 
@@ -21,4 +21,60 @@ if (4 > N):
     sys.exit(1)
 
 board = [[0 for i in range(0, N)] for j in range(0, N)]
-print(board)
+
+def nQueen(board, col):
+    """recur place queen in every col"""
+    if col == N:
+        """queens are placed call solution"""
+        solution(board)
+        return True
+
+    """place queen at every sqr in the row recur if isSafe"""
+    for row in range(0, N):
+        """if no queen threaten another"""
+        if isSafe(board, row, col):
+            """place queen in current square"""
+            board[row][col] = 1
+            """recur for next row"""
+            nQueen(board, col + 1)
+            """backtrack and remove the queen from the current square"""
+            board[row][col] = 0
+    return False
+
+def isSafe(board, row, col):
+    """Function to check if two queens threaten each other or not """
+    for x in range(0, N):
+        """rtn false if two queens are in same row or col"""
+        if board[row][x] == 1 or board[x][col] == 1:
+            return False
+    """return false if two queens share the same `\` diagonal"""
+    (i, j) = (row, col)
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i = i - 1
+        j = j - 1
+    """return false if two queens share the same `/` diagonal"""
+    (i, j) = (row, col)
+    while i >= 0 and j < N:
+        if board[i][j] == 1:
+            return False
+        i = i - 1
+        j = j + 1
+    """all is safe so True"""
+    return True
+
+def solution(board):
+    """print all queen positions once solved"""
+
+    Positions = []
+
+    for row in range(0, N):
+        for col in range(0, N):
+            if board[row][col] == 1:
+                Positions.append([row, col])
+
+    print(Positions)
+
+
+nQueen(board, 0)
