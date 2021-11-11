277fdac94b5ba038928da345db2883482ec17ef0
Corbin <forstupidityonly@gmail.com>
2021-11-11 16:43:16 -0600

    I hope this changed the nsfw commit message it

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,18 +8,12 @@ request.get(url, (error, response, body) => {
         return console.log(error);
     }
     const characters = JSON.parse(body).characters;
-    const charDict = {};
     for (const character of characters) {
         request(character, (errori, responsei, bodyi) => {
             if (error) {
                 return console.log(error);
             }
-            charDict[character] = JSON.parse(bodyi).name;
-            if (Object.values(charDict).length === characters.length) {
-                for (const i in characters) {
-                    console.log(charDict[i]);
-                }
-            }
+            console.log(JSON.parse(bodyi).name);
         });
     }
 });
