91277d0b2af38234bcd59658516abe9efaeb52cf
Corbin <forstupidityonly@gmail.com>
2021-11-11 16:40:48 -0600

    immmm so frestrated omg

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,18 +3,23 @@
 const request = require('request');
 const myArg = process.argv.slice(2);
 const url = `https://swapi-api.hbtn.io/api/films/${myArg}/`;
-request.get(url, async (error, response, body) => {
-  const characters = JSON.parse(body).characters;
-  for (const character of characters) {
-    const c = await new Promise((resolve, reject) => {
-      request(character, (errori, responsei, bodyi) => {
-        if (errori) {
-          return console.log(errori);
-        } else {
-          resolve(JSON.parse(bodyi).name);
-        }
-      });
-    });
-    console.log(c);
-  }
+request.get(url, (error, response, body) => {
+    if (error) {
+        return console.log(error);
+    }
+    const characters = JSON.parse(body).characters;
+    const charDict = {};
+    for (const character of characters) {
+        request(character, (errori, responsei, bodyi) => {
+            if (error) {
+                return console.log(error);
+            }
+            charDict[character] = JSON.parse(bodyi).name;
+            if (Object.values(charDict).length === characters.length) {
+                for (const i in characters) {
+                    console.log(charDict[i]);
+                }
+            }
+        });
+    }
 });
