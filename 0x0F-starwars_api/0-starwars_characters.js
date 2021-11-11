cd03a13ffd8fd1c87ffab02e511ffa04ff27a9c4
Corbin <forstupidityonly@gmail.com>
2021-11-11 16:22:07 -0600

    expected error my asssss

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,14 +2,14 @@
 
 const request = require('request');
 const myArg = process.argv.slice(2);
-let url = `https://swapi-api.hbtn.io/api/films/${myArg}/`
+const url = `https://swapi-api.hbtn.io/api/films/${myArg}/`;
 request.get(url, async (error, response, body) => {
   const characters = JSON.parse(body).characters;
   for (const character of characters) {
     const c = await new Promise((resolve, reject) => {
       request(character, (errori, responsei, bodyi) => {
         if (errori) {
-          return console.log(errori)
+          return console.log(errori);
         } else {
           resolve(JSON.parse(bodyi).name);
         }
